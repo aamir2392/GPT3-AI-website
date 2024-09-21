0f4e80cd0c0f1064bdf2c2d92f03b9af46e12b20
Aamir Habib <109647372+aamir2392@users.noreply.github.com>
2024-09-21 16:59:00 +0500

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -36,14 +36,14 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>Muhammad &lt;Aamir/&gt; Bin Habib</p>
+        <p>+92-3169344927</p>
+        <p>aamirab1017@gmail.com</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2024 GPT-3. All rights reserved by &lt;Aamir/&gt; Habib</p>
     </div>
   </div>
 );
